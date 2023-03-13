8267bb14425d9f3acfeff294aadfe1aa81783060
Duc <umarzahid021@gmail.com>
2023-03-13 22:43:58 +0530

    Deleted all files for refactor

--- src/database.js
+++
@@ -1,9 +0,0 @@
-import { collection, setDoc, doc } from "firebase/firestore"
-import { onAuthStateChanged } from "firebase/auth"
-import { auth, db } from "./index"
-
-onAuthStateChanged(auth, (user) => {
-	if (user) {
-		setDoc(doc(db, 'users', user.uid), { merge: true })
-	}
-})
