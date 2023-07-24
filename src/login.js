723987ee1f93ec1f62f2b38f9a3bfa96116ff3f6
Duc <umarzahid021@gmail.com>
2023-07-24 17:22:38 +0530

    Rewrote login code to be more compact

--- src/login.js
+++ src/login.js
@@ -2,34 +2,17 @@ import { auth, db } from './index'
 import { setDoc, doc } from 'firebase/firestore'
 import {
 	GoogleAuthProvider,
-	signInWithRedirect,
-	onAuthStateChanged,
-	signOut
+	signInWithPopup
 } from 'firebase/auth'
-
-// Google Sign in function. More modular approach than alternatives
-const googleSignIn = async (user) => {
-	try {
-		const provider = new GoogleAuthProvider()
-		const result = await signInWithRedirect(auth, provider)
-		document.location = '/'
-	} catch (e) {
-		console.log(e.code, e.message)
-	}
-}
 
 const googleSignInBtn = document.querySelector('#google-sign-in')
 
-// called when user logs in or out
-onAuthStateChanged(auth, (user) => {
-	// document.location = '/'
-	if (user) {
-		// create document for user
+googleSignInBtn.addEventListener('click', () => {
+	const provider = new GoogleAuthProvider()
+	signInWithPopup(auth, provider).then(result => {
+		const user = result.user
 		setDoc(doc(db, 'users', user.uid), { merge: true })
 		console.log(user.uid)
-		signOutBtn.removeAttribute('hidden')
-		googleSignInBtn.setAttribute('hidden', true)
-	}
+		window.location.href = '/'
+	})
 })
-
-googleSignInBtn.addEventListener('click', googleSignIn)
